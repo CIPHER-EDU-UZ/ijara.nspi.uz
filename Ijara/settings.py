145f5a0d7a047c983a0194571a9fa3fea14df613
CIPHER-EDU <ciphereduuz@gmail.com>
2023-08-22 14:20:32 +0500

    add new html

--- Ijara/settings.py
+++ Ijara/settings.py
@@ -116,19 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS=[
-#     BASE_DIR/ "static"
-# ]
+STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR/ "static"
+]
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # settings.py
-
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static'),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import *
-urlpatterns = [
 
+from .views import *
+
+urlpatterns = [
+    path("", home, name="home"),
 ]
