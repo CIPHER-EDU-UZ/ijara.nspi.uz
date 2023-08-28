163c5964de6770b288c48b019c6e6cb880d082d3
CIPHER-EDU <ciphereduuz@gmail.com>
2023-08-29 00:13:43 +0500

    add

--- core/forms.py
+++ core/forms.py
@@ -29,3 +29,23 @@ class ArizaForm(forms.ModelForm):
     class Meta:
         model = Ariza
         fields = '__all__'
+        widgets = {
+            'ism': forms.TextInput(attrs={'class': 'tt-form-control'}),
+            'familiya': forms.TextInput(attrs={'class': 'tt-form-control'}),
+            'sharifi': forms.TextInput(attrs={'class': 'tt-form-control'}),
+            'mamlakat': forms.Select(attrs={'class': 'tt-form-control'}),
+            'viloyat': forms.Select(attrs={'class': 'tt-form-control'}),
+            'tuman': forms.TextInput(attrs={'class': 'tt-form-control'}),
+            'jinsi': forms.Select(attrs={'class': 'tt-form-control'}),
+            'pasport': forms.TextInput(attrs={'class': 'tt-form-control'}),
+            'pasport_num': forms.NumberInput(attrs={'class': 'tt-form-control'}),
+            'manzil': forms.TextInput(attrs={'class': 'tt-form-control'}),
+            'fakultet': forms.Select(attrs={'class': 'tt-form-control'}),
+            'yunalish': forms.TextInput(attrs={'class': 'tt-form-control'}),
+            'grux': forms.TextInput(attrs={'class': 'tt-form-control'}),
+            'kurs': forms.Select(attrs={'class': 'tt-form-control'}),
+            'telefon': forms.NumberInput(attrs={'class': 'tt-form-control'}),
+            'uploaded_file': forms.FileInput(attrs={'class': 'tt-form-control'}),
+            'coment': forms.FileInput(attrs={'class': 'tt-form-control'}),
+        }
+
